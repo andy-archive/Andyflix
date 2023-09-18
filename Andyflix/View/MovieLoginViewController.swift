4e2b5579b1b8348e5b70b875f7950787561b9932
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-09-18 17:49:12 +0900

    init: project initial settings

---
+++ Andyflix/View/MovieLoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieLoginViewController.swift
+//  Andyflix
+//
+//  Created by Taekwon Lee on 2023/09/18.
+//
+
+import UIKit
+
+final class MovieLoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+    
+}
---
+++ Andyflix/View/RandomPhotoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RandomPhotoViewController.swift
+//  Andyflix
+//
+//  Created by Taekwon Lee on 2023/09/18.
+//
+
+import UIKit
+
+final class RandomPhotoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBrown
+    }
+    
+}
